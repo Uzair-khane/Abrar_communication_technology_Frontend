9447e030c2a06405ffbdf4044a805738bb1044f0
Uzair-khane <uzairkhanar123@gmail.com>
2025-12-26 16:13:35 +0500

    fitst commit

---
+++ Abrar_communiction_frontend/app/pages/about.vue
@@ -0,0 +1,9 @@
+<script  setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>about page</h1>
+    </div>
+</template>
---
+++ Abrar_communiction_frontend/app/pages/index.vue
@@ -0,0 +1,7 @@
+<script  setup>
+
+</script>
+
+<template>
+    <h1 class="text-slate-100">home page</h1>
+</template>
